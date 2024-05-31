37051a93c8d3ad1887d9053e74d4cc4eaf935fdb
mitch <mgregoris2@myseneca.ca>
2024-05-31 15:32:07 -0400

    file creation

---
+++ lab3d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Author: Mitchell Gregoris
+Student ID: 133349191
+Email: mgregoris2@myseneca.ca
+Program Name: lab3c.py
+Date: 2024-05-14
+Description: function operations
+'''
+
