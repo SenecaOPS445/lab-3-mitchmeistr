b160bd8898f6bfe175464860239a5a5751219736
mitch <mgregoris2@myseneca.ca>
2024-05-31 22:07:38 -0400

    module 3d completed

--- lab3d.py
+++ lab3d.py
@@ -8,3 +8,13 @@ Date: 2024-05-14
 Description: function operations
 '''
 
+import subprocess
+
+def free_space():
+    command = "df -h | grep '/$' | awk '{print $4}'"
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output = p.communicate()
+    # The above stdout is stored in bytes
+    # Convert stdout to a string and strip off the newline characters
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
